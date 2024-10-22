14ad63312854e4e530309a065e3a3fb40f0966d3
divaluna <diva.luna@torontomu.ca>
2024-10-22 00:40:44 +0000

    plswork

--- main.js
+++ main.js
@@ -1,35 +1,37 @@
-///prompt function to receive name input
-function namePrompt(){
-    let inputName = prompt("Please enter your name: ");
-    if (inputName) {
-        document.getElementById("name").textContent = "Hello, " + inputName + "!";
-    } else {
-        document.getElementById("name").textContent = "I don't know who you are! :(";
+window.onload = function() {
+    ///prompt function to receive name input
+    function namePrompt(){
+        let inputName = prompt("Please enter your name: ");
+        if (inputName) {
+            document.getElementById("name").textContent = "Hello, " + inputName + "!";
+        } else {
+            document.getElementById("name").textContent = "I don't know who you are! :(";
+        }
     }
-}
 
-///image size function to toggle enlargement
-function imageSize(){
+    ///image size function to toggle enlargement
+    function imageSize(){
+        const divaPhoto = document.getElementById("divaPhoto");
+       divaPhoto.classList.toggle("enlarged");
+    }
+
+    ///event listener for button
+    const button = document.getElementById("nameButton");
+    button.addEventListener("click", namePrompt);
+
+    ///event listener for photo
     const divaPhoto = document.getElementById("divaPhoto");
-    divaPhoto.classList.toggle("enlarged");
-}
+    divaPhoto.addEventListener("click",imageSize);
 
-///event listener for button
-const button = document.getElementById("nameButton");
-button.addEventListener("click", namePrompt);
+    ///adjust colour on mouse over
+    function adjustColour(item) {
+        item.style.backgroundColor = "#857575cc";
+       item.style.color = "#fff";
+    }
 
-///event listener for photo
-const divaPhoto = document.getElementById("divaPhoto");
-divaPhoto.addEventListener("click",imageSize);
-
-///adjust colour on mouse over
-function adjustColour(item) {
-    item.style.backgroundColor = "#857575cc";
-    item.style.color = "#fff";
-}
-
-///adjust colour on mouse off
-function resetColour(item) {
-    item.style.backgroundColor = "#ffffff99";
-    item.style.color = "#3a3434";
+    ///adjust colour on mouse off
+    function resetColour(item) {
+        item.style.backgroundColor = "#ffffff99";
+        item.style.color = "#3a3434";
+    }
 }
